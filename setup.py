272bfc6f69ccc7a3c6d74f73172729e87be3ae85
Jacob Lurye <jlurye@college.harvard.edu>
2018-04-30 17:32:27 -0400

    Added dependencies to setup.py

--- setup.py
+++ setup.py
@@ -11,6 +11,12 @@ version = re.search(
   re.M
   ).group(1)
 
+install_requires = [
+  'numpy>=1.14.3',
+  'scikit-image>=0.13.1',
+  'scikit-video>=1.1.10'
+]
+
 descr = "Convert and display images and videos as string representations."
 long_descr = descr
 
@@ -20,6 +26,7 @@ setup(
   entry_points = {
       "console_scripts": ['imprint = imprint.cli:main']
       },
+  install_requires = install_requires,
   version = version,
   description = descr,
   long_description = long_descr,
