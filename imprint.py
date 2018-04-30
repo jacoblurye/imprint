00a010417913f9e0cf213cb22e23053bd1d5080a
Jacob Lurye <jlurye@college.harvard.edu>
2018-04-30 02:42:11 -0400

    Refactoring

--- imprint.py
+++ imprint.py
@@ -1,6 +1,6 @@
 import numpy as np
 from skimage.transform import resize
-from imageio import imread
+from skimage.io import imread
 from skvideo.io import vreader
 
 # Symbols are listed in order from "darkest" to "lightest"
@@ -17,7 +17,7 @@ class ImgPrinter:
     self._inc = bitdepth / len(symbols)
     self._ptoc_vec = np.vectorize(self._pixel_to_char)
 
-  def img_to_str(self, img, max_width=200):
+  def img_to_str(self, img, max_width=150):
     """
       Convert img (a matrix of rgb-triples representing an image)
       into its string representation.
@@ -26,13 +26,16 @@ class ImgPrinter:
       img = imread(img)
 
     # Convert image to black and white,
-    # dropping every other row to prevent vertical stretch
     tall_bw_img = img.mean(axis=2)
-    bw_img = resize(tall_bw_img, (img.shape[0] // 2, img.shape[1]))
+
+    # Remove every other row to prevent vertical stretching
+    bw_img = tall_bw_img[::2]
+
+    # Decrease image resolution if necessary
     if max_width <= img.shape[1]:
       max_height = round(bw_img.shape[0] * (max_width / img.shape[1]))
-      lo_res_shape = (max_height, max_width)
-      bw_img = resize(bw_img, lo_res_shape)
+      smaller_shape = (max_height, max_width)
+      bw_img = resize(bw_img, smaller_shape, mode="constant")
 
     # Convert pixels to corresponding symbols
     chr_img = self._ptoc_vec(bw_img)
@@ -48,7 +51,7 @@ class ImgPrinter:
   def _pixel_to_char(self, pixel):
     """
       Assuming self.symbols are listed from "darkest" to "brightest",
-      replace pixel appropriately according to its light intensity.
+      replace pixel with symbol according to its light intensity.
     """
     thresh = self._inc
     for char in self.symbols:
