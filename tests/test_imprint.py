eccbda4a4b4652a2a7ff0b777a0ada3a19079fd5
Jacob Lurye <jlurye@college.harvard.edu>
2019-03-01 00:21:49 -0500

    Add initial ASCIIVideo test

--- tests/test_imprint.py
+++ tests/test_imprint.py
@@ -22,12 +22,13 @@ class TestASCIIMedia(TestCase):
         self.assertIs(video_media._PrinterClass, ASCIIVideo)
 
 
+def make_uint8_matrix(mat: list):
+    return np.array(mat, dtype=np.uint8)
+
+
 class TestASCIIImage(TestCase):
     def setUp(self):
         self.image = ASCIIImage(mock_image_path, symbols='abc', bitdepth=3)
-
-    def make_imdata(self, im: list):
-        return np.array(im, dtype=np.uint8)
 
     def test_pixel_to_char(self):
         pixels = [0, 1, 2]
@@ -36,20 +37,20 @@ class TestASCIIImage(TestCase):
 
     def test_image_to_string(self):
         # Image gets vertically squished and values get averaged
-        imdata = self.make_imdata([[0, 2, 1], [1, 2, 0]])
+        imdata = make_uint8_matrix([[0, 2, 1], [1, 2, 0]])
         string = self.image._image_to_string(imdata, max_width=None)
         self.assertEqual(string, 'bcb')
 
     def test_image_to_string_max_width(self):
         # Image's width and height will be cut in half to achieve max width,
         # then the image's height will be ~halved again (vertical squish)
-        imdata = self.make_imdata([[0, 2]] * 8)
+        imdata = make_uint8_matrix([[0, 2]] * 8)
         string = self.image._image_to_string(imdata, max_width=1)
         self.assertEqual(string, 'b\nb')
 
     def test_image_to_string_corner_cases(self):
         # A value greater than configured bitdepth
-        imdata = self.make_imdata([[100]])
+        imdata = make_uint8_matrix([[100]])
         string = self.image._image_to_string(imdata, max_width=None)
         self.assertEqual(string, 'c')
 
@@ -58,3 +59,13 @@ class TestASCIIImage(TestCase):
         imdata = np.array([[-1]])
         string = self.image._image_to_string(imdata, max_width=None)
         self.assertEqual(string, 'a')
+
+
+class TestASCIIVideo(TestCase):
+    def setUp(self):
+        self.video = ASCIIVideo(mock_video_path, symbols='abc', bitdepth=3)
+
+    def test_video_to_string(self):
+        vdata = make_uint8_matrix([[[0, 1], [0, 1]], [[1, 2], [1, 2]]])
+        string = list(self.video._video_to_string(vdata, max_width=None))
+        self.assertEqual(string, ['ab', 'bc'])
